262b20bfa15b4830cf7356c797b5363ef0d79094
swapnilpawar07072000 <swapnil.pawar07072000@gmail.com>
2024-06-22 22:27:42 +0530

    Changed the Hostname

--- src/calendar/calendarIndex.js
+++ src/calendar/calendarIndex.js
@@ -6,9 +6,11 @@ import interactionPlugin from "@fullcalendar/interaction" // needed for dayClick
 import listPlugin from '@fullcalendar/list';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Input } from "reactstrap";
+const moment = require("moment");
 
 var stored_event_list = "";
 
+// const hostname = 'http://localhost:3003'; 
 const hostname = 'https://todocalendarserver.onrender.com';
 
 async function getJSONFile(){
@@ -27,6 +29,7 @@ if(stored_event_list["data"] === undefined){
 }
 var stored_list = Object.values((({ last_id, ...o }) => o)(stored_event_list["data"])); //Object.values(stored_event_list);
 // console.log(stored_event_list);
+// console.log(stored_list);
 const initialState = {
         event_list: stored_event_list,
         stored_list: stored_list,
@@ -46,6 +49,9 @@ const initialState = {
 export default function Calendar() {
     const calendarRef = useRef();
     const [action, setAction] = useState(initialState);
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(new Date());
+
 
     const toggle = (reset='no') => {
         if(reset == 'yes'){
@@ -74,38 +80,40 @@ export default function Calendar() {
         var event_list = action.event_list;
         event_list["last_id"] = stored_event_list.last_id;
         const url = hostname+"/api/updateJSONFile";
-        console.log(url);
-        console.log(event_list);
         const resp = await fetch(url, {
                 method: 'POST',
                 headers: { 
                     'Accept': 'application/json, text/plain, */*',
                     "Content-Type": "application/json" },
                 body: JSON.stringify(event_list),
-            })
-        setAction({...action, event_list: resp});
+            });
+        var data = await resp.json();
+        var stored_list = Object.values((({ last_id, ...o }) => o)(data["data"]));
+        action.stored_list = stored_list;
+        stored_event_list = data;
+        action.event_list = data;
     }       
 
     const setEvent = async() => {
         const api = calendarRef.current.getApi();
-        if(action.event_list["data"] === undefined){
-            action.event_list["data"] = {};
-        }
         var startDate = action.startDate;
         var endDate = action.endDate;
         var event_desc = action.event_list;
+        var is_new = false;
         if(action.event_id === undefined || action.event_id === ''){
+            is_new = true;
             var last_id = action.event_list.last_id;
             action.event_id = last_id;
         }
+        
         var obj = {id: action.event_id, title: action.title, description: action.description, 
-            start: new Date(startDate), end: new Date(endDate), 
+            start: (new Date(startDate+"T"+action.startTime)).toISOString(), 
+            end: (new Date(endDate+"T"+action.endTime)).toISOString(),
+            start_date: new Date(startDate), end_date: new Date(endDate),
             start_time: action.startTime, end_time: action.endTime,
             rendering: 'background', block: true};
-        if(event_desc["data"]===undefined || event_desc["data"][action.event_id] === undefined){
-            if(event_desc["data"]===undefined){
-                event_desc["data"] = {};
-            }
+        // if(event_desc["data"]===undefined || event_desc["data"][action.event_id] === undefined){
+        if(is_new){
             event_desc["data"][action.event_id] = obj;
             setAction({...action, event_list: event_desc, event_id: ''});
             stored_event_list.last_id += 1;
@@ -115,8 +123,7 @@ export default function Calendar() {
         }
         getEventList(action.event_list);
         await updateJSONFile();
-        toggle("yes");
-        window.location.reload();
+        // toggle("yes");
     }
 
     const deleteEventID = async(event_id) => { 
@@ -131,9 +138,11 @@ export default function Calendar() {
         const resp_1 = await resp.json();
         // console.log(resp_1);
         const stored_list = Object.values((({ last_id, ...o }) => o)(resp_1["data"]));
-        setAction({...action, event_list: resp_1, stored_list: stored_list});
+        action.stored_list = stored_list;
+        action.event_list = resp_1;
+        // setAction({...action, event_list: resp_1, stored_list: stored_list});
         toggle("yes");
-        window.location.reload();
+        // window.location.reload();
     };
 
     function getEventList(stored_event_list){
@@ -145,10 +154,6 @@ export default function Calendar() {
     }
 
     const handleEventClick = (args) => {
-        if(action.event_list["data"] === undefined){
-            action.event_list["data"] = {};
-        }
-        // console.log(args.event.id);
         var eventInfo = action.event_list["data"][args.event.id];
         var endDate = new Date(eventInfo.end);
         endDate = (endDate.toISOString().split('T')[0]) // +" "+ (endDate.toISOString().split('T')[1]).split('.')[0]);
@@ -162,25 +167,26 @@ export default function Calendar() {
         
     }
     const handleDateClick = (args) => {
-        if(action.event_list["data"] === undefined){
-            action.event_list["data"] = {};
-        }
         var date = new Date(args.dateStr);
         var startDate = date.toISOString().split('T')[0];
         var endDate = startDate;
+        action.event_id = "";
         setAction({...action, modal: !action.modal, date_data: args,
-            title: '', description: '', endDate: endDate, startDate: startDate, startTime: "12:00", endTime: "12:00"});
+            title: '', description: '', endDate: endDate, 
+            startDate: startDate, startTime: "00:00", endTime: "00:00"});
     }
 
     const handleChange = (event) => {
         const value = event.target.value;
         const name = event.target.name;
+        // console.log(name, value);
         setAction({...action, [name]: value});
     };
 
     let button;
     let deleteButton_ = "";
-    if(action.event_id && (action.event_list["data"] !== undefined) && (action.event_list["data"][action.event_id] !== undefined)){
+    if(action.event_id && (action.event_list["data"] !== undefined) && 
+        (action.event_list["data"][action.event_id] !== undefined)){
         button = <Button color="primary" onClick={setEvent}>Update</Button>
         deleteButton_ = <Button color="danger" onClick={e => deleteEventID(action.event_id)}>Delete</Button>
     }else{
@@ -200,6 +206,7 @@ export default function Calendar() {
         }else{
             startDate = startDate.toDateString();
         }
+        // console.log(moment(startDate).format('YYYY/MM/DD hh:mm'));
         readAbleDate = <p>{startDate}</p>
     }
 
@@ -229,7 +236,7 @@ export default function Calendar() {
                 <ModalBody>
                     <div>
                         <Input type="text" name="title" placeholder='Please enter Title....' onChange={handleChange} value={action.title || ''} /><br/>
-                        <Input type="date" name="startDate" placeholder='Please select Start Date....' onChange={handleChange} value={action.startDate || ''} /><br/>
+                        <Input type="date"  name="startDate" placeholder='Please select Start Date....' onChange={handleChange} value={action.startDate || ''} /><br/>
                         <Input type="time" name="startTime" placeholder='Please select Start Time....' onChange={handleChange} value={action.startTime || ''} /><br/>
                         <Input type="date" name="endDate" placeholder='Please select End Date....' onChange={handleChange} value={action.endDate || ''} /><br/>
                         <Input type="time" name="endTime" placeholder='Please select End Time....' onChange={handleChange} value={action.endTime || ''} /><br/>
